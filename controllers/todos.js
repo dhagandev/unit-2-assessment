aff5146c9f83073138b126241850ce57a350e3d2
dhagandev <dhagan@utexas.edu>
2019-05-31 16:46:45 -0500

    Bonus

--- controllers/todos.js
+++ controllers/todos.js
@@ -1,15 +1,34 @@
-var Todos = require('../models/todos');
+var Todo = require('../models/todos');
 
 module.exports = {
 	index,
-	create
+	create,
+	complete,
+	update,
+	delete: deleteTodo
 }
 
 function index(req, res, next) {
-	res.render('index', {Todos});
+	console.log(req.body)
+	res.render('index', {Todo});
 }
 
 function create(req, res, next) {
-	Todos.push({todo: req.body.newTodo, done: false});
+	Todo.push({todo: req.body.newTodo, done: false});
+	res.redirect("/");
+}
+
+function complete(req, res, next) {
+	console.log(req.body)
+	console.log(req.params.id)
+	res.render('index', {Todo})
+}
+
+function update(req, res, next) {
+
+}
+
+function deleteTodo(req, res, next) {
+	Todo.splice(req.params.idx, 1);
 	res.redirect("/");
 }
--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ var todosCtrl = require('../controllers/todos')
 /* GET home page. */
 router.get('/', todosCtrl.index);
 router.post('/create', todosCtrl.create);
+// router.get('/complete', todosCtrl.complete);
+// router.put('/update', todosCtrl.update);
+router.delete('/:idx', todosCtrl.delete)
 
 module.exports = router;
